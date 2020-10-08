a939549cc72f3e038024b805e2071e58ad593b24
josuke-higashikata <bowles.joshp@gmail.com>
2020-10-08 18:03:25 -0400

    Added the updated NES cartridge stuff

--- nes_cartridge.h
+++ nes_cartridge.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "nes_ppu.h"
 
+/* Clock information here */
+const float NES_MASTER_CLOCK    = 1 / (21.47727273f * 100000),  //21.477... MHz to seconds
+            NES_PPU_CLOCK       = (NES_MASTER_CLOCK * 4),       // Divide MSC by 4 and 12 (in this case, multiply to increase duration in seconds)
+            NES_CPU_CLOCK       = (NES_MASTER_CLOCK * 12);
+
 /* NES Cartridge data */
 typedef struct _nes_cartridge
 {
@@ -17,6 +22,8 @@ _nes_cartridge nes_cartridge;
 uint8_t (*PEEK_MAPPER)(uint16_t);
 void    (*POKE_MAPPER)(uint16_t, uint8_t);
 
+/* OAMDMA (copy from CPU address space to OAM from $XX00 - $XXFF) */
+void EXEC_OAMDMA(uint8_t oam_copy_addr_hb);
 
 /* Mapper 000 PEEK */
 uint8_t PEEK_000(uint16_t addr)
@@ -30,7 +37,6 @@ uint8_t PEEK_000(uint16_t addr)
         USE_REGS((addr & 0x7), 0, 0x0);
         return nes_ppu.PPU_registers[(addr & 0x7)];
     }
-    
     /* Mirror if PRG_ROM is only 16 KiB */
     if (addr >= 0x8000)
     {
@@ -48,16 +54,25 @@ void POKE_000(uint16_t addr, uint8_t data)
     if (addr >= 0x0 && addr < 0x2000)
         nes_cartridge.nes_mem[(addr & 0x07FF)] = data;
     /* PPU Registers */
-    if (addr >= 0x2000 && addr < 0x4000)
+    else if (addr >= 0x2000 && addr < 0x4000)
         USE_REGS((addr & 0x7), 1, data);
+    else if (addr == 0x4014)
+        EXEC_OAMDMA(data);
     /* Mirror if PRG_ROM is only 16 KiB */
-    if (addr >= 0x8000)
+    else if (addr >= 0x8000)
     {
         if (nes_cartridge.PRG_ROM_size == 0x4000)
             nes_cartridge.nes_mem[(addr & 0x3FFF) + 0x8000] = data;
         else
             nes_cartridge.nes_mem[addr] = data;
     }
+}
+
+/* OAMDMA (copy from CPU address space to OAM from $XX00 - $XXFF) */
+void EXEC_OAMDMA(uint8_t oam_copy_addr_hb)
+{
+    uint16_t oam_copy_addr = (uint16_t) oam_copy_addr_hb << 8;
+    memcpy(nes_ppu.PPU_OAM_Bytes, &nes_cartridge.nes_mem[oam_copy_addr], 256);
 }
 
 /* Mapper 000 */
@@ -67,10 +82,17 @@ void mapper_000(FILE * rom)
     PEEK_MAPPER = PEEK_000;
     POKE_MAPPER = POKE_000;
 
-    /* Program ROM, loaded in the range $8000-$FFFF */
+    /* Program ROM, loaded in CPU bus in the range $8000-$FFFF */
     if (fread(&nes_cartridge.nes_mem[0x8000], sizeof(uint8_t), nes_cartridge.PRG_ROM_size, rom) != nes_cartridge.PRG_ROM_size)
     {
         fprintf(stderr, "error: Failed to copy PRG-ROM: %s. exiting\n", strerror(errno));
+        return;
+    }
+
+    /* Character ROM, loaded in PPU bus */
+    if (fread(nes_ppu_bus.mem, sizeof(uint8_t), nes_cartridge.CHR_ROM_size, rom) != nes_cartridge.CHR_ROM_size)
+    {
+        fprintf(stderr, "error: Failed to copy CHR-ROM: %s. exiting\n", strerror(errno));
         return;
     }
 
@@ -278,3 +300,12 @@ void (*mapper[256])(FILE*) = {
     mapper_NULL,
     mapper_NULL
 };
+
+/* Tick function (1 CPU cycle = 3 PPU cycles) */
+static inline void tick()
+{
+    PPU_tick();
+    PPU_tick();
+    PPU_tick();
+    //APU_tick();
+}
